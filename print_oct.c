f4a773397bf5b6a05209c4a24fb1e40640dcd5b1
Abdeljalil37 <widadpop@gmail.com>
2023-07-23 16:42:35 +0100

    fix

--- print_oct.c
+++ print_oct.c
@@ -44,6 +44,33 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 }
 
 /**
+ * fill_long_oct_array - calculates a long octal number
+ *
+ * @bnr: array where is stored the binary.
+ * @oct: array where is stored the octal.
+ *
+ * Return: binary array.
+ */
+char *fill_long_oct_array(char *bnr, char *oct)
+{
+	int op, i, j, ioct, limit;
+
+	oct[22] = '\0';
+	for (i = 63, ioct = 21; i >= 0; i--, ioct--)
+	{
+		if (i > 0)
+			limit = 4;
+		else
+			limit = 1;
+		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+			op = ((bnr[i] - '0') * j) + op;
+		i++;
+		oct[ioct] = op + '0';
+	}
+	return (oct);
+}
+
+/**
  * fill_oct_array - writes the character c to stdout
  *
  * @bnr: array where is stored the binary.
@@ -69,7 +96,32 @@ char *fill_oct_array(char *bnr, char *oct)
 	}
 	return (oct);
 }
+/**
+ * fill_short_oct_array - calculates a short octal number
+ *
+ * @bnr: array where is stored the binary.
+ * @oct: array where is stored the octal.
+ *
+ * Return: binary array.
+ */
+char *fill_short_oct_array(char *bnr, char *oct)
+{
+	int op, i, j, ioct, limit;
 
+	oct[6] = '\0';
+	for (i = 15, ioct = 5; i >= 0; i--, ioct--)
+	{
+		if (i > 0)
+			limit = 4;
+		else
+			limit = 1;
+		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+			op = ((bnr[i] - '0') * j) + op;
+		i++;
+		oct[ioct] = op + '0';
+	}
+	return (oct);
+}
 /**
  * prinloct - prints long decimal number in octal
  * @arguments: input number
@@ -113,91 +165,7 @@ int prinloct(va_list arguments, char *buf, unsigned int ibuf)
 	free(octal);
 	return (count);
 }
-/**
- * prinnoct - print the number in octal begining with zero
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
- */
-int prinnoct(va_list arguments, char *buf, unsigned int ibuf)
-{
-	int int_input, i, isnegative, count, first_digit;
-	char *octal, *binary;
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
-	if (int_input == 0)
-	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
-	ibuf = handl_buf(buf, '0', ibuf);
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	octal = malloc(sizeof(char) * (11 + 1));
-	octal = fill_oct_array(binary, octal);
-	for (first_digit = i = count = 0; octal[i]; i++)
-	{
-		if (octal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, octal[i], ibuf);
-			count++;
-		}
-	}
-	free(binary);
-	free(octal);
-	return (count + 1);
-}
-#include "main.h"
 
-/**
- * prinhoct - prints long decimal number in octal
- * @arguments: input number
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed.
- */
-int prinhoct(va_list arguments, char *buf, unsigned int ibuf)
-{
-	short int int_input, i, isnegative, count, first_digit;
-	char *octal, *binary;
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
-	if (int_input == 0)
-	{
-		ibuf = handl_buf(buf, '0', ibuf);
-		return (1);
-	}
-	if (int_input < 0)
-	{
-		int_input = (int_input * -1) - 1;
-		isnegative = 1;
-	}
 
-	binary = malloc(sizeof(char) * (16 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 16);
-	octal = malloc(sizeof(char) * (6 + 1));
-	octal = fill_short_oct_array(binary, octal);
-	for (first_digit = i = count = 0; octal[i]; i++)
-	{
-		if (octal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
-		{
-			ibuf = handl_buf(buf, octal[i], ibuf);
-			count++;
-		}
-	}
-	free(binary);
-	free(octal);
-	return (count);
-}
