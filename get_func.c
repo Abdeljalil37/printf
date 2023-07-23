c868f5e6866c97dc14607c3d5352143a02175b6f
Abdeljalil37 <widadpop@gmail.com>
2023-07-23 17:39:15 +0100

    betty whitespace

--- get_func.c
+++ get_func.c
@@ -12,7 +12,7 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 		{"b", print_bnr}, {"u", print_unt},
 		{"x", print_hex}, {"X", print_upx},
 		{"i", print_int}, {"d", print_int},
-		{"%", print_prg}, {"o", print_oct}, 
+		{"%", print_prg}, {"o", print_oct},
 		{NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
