2f73b6286ce7fd0f85e524bb8b24cccb82b4b210
Abdeljalil37 <widadpop@gmail.com>
2023-07-24 23:24:07 +0100

    indent spaces and white space for betty

--- print_unisigned.c
+++ print_unisigned.c
@@ -10,19 +10,19 @@
 
 int print_unt(va_list arguments, char *buf, unsigned int ibuf)
 {
-        unsigned int int_in, int_tmp, i, div;
+	unsigned int int_in, int_tmp, i, div;
 
-        int_in = va_arg(arguments, unsigned int);
-        int_tmp = int_in;
-        div = 1;
-        while (int_tmp > 9)
-        {
-                div *= 10;
-                int_tmp /= 10;
-        }
-        for (i = 0; div > 0; div /= 10, i++)
-        {
-                ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
-        }
-        return (i);
+	int_in = va_arg(arguments, unsigned int);
+	int_tmp = int_in;
+	div = 1;
+	while (int_tmp > 9)
+	{
+		div *= 10;
+		int_tmp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
 }
