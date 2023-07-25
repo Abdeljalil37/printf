c5ff1092feaf68f3f7c5ff782cecf0996494a938
usri rachid <shixymanflowx@gmail.com>
2023-07-25 02:52:54 -0700

    fix hot

--- print_adresse.c
+++ print_adresse.c
@@ -8,6 +8,8 @@
  *
  * Return: number of chars printed.
  */
+
+
 int print_add(va_list arguments, char *buf, unsigned int ibuf)
 {
 	void *add;
@@ -18,11 +20,9 @@ int print_add(va_list arguments, char *buf, unsigned int ibuf)
 
 	add = (va_arg(arguments, void *));
 	if (add == NULL)
-	{
 		for (i = 0; nill[i]; i++)
 			ibuf = handl_buf(buf, nill[i], ibuf);
 		return (5);
-	}
 	int_input = (intptr_t)add;
 	isnegative = 0;
 	if (int_input < 0)
@@ -35,13 +35,11 @@ int print_add(va_list arguments, char *buf, unsigned int ibuf)
 	ibuf = handl_buf(buf, '0', ibuf);
 	ibuf = handl_buf(buf, 'x', ibuf);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
-	{
 		if (hexadecimal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
 			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
 			count++;
-	}
 	free(binary);
 	free(hexadecimal);
 	return (count + 2);
