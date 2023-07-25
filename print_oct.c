24e61be2a93e7b7c86beff20cac38ca3f92d24ee
usri rachid <shixymanflowx@gmail.com>
2023-07-25 03:05:42 -0700

    fix hot

--- print_oct.c
+++ print_oct.c
@@ -15,67 +15,55 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 	int_input = va_arg(arguments, int);
 	isnegative = 0;
 	if (int_input == 0)
-	{
 		ibuf = handl_buf(buf, '0', ibuf);
 		return (1);
-	}
 	if (int_input < 0)
-	{
 		int_input = (int_input * -1) - 1;
 		isnegative = 1;
-	}
 	binary = malloc(sizeof(char) * (32 + 1));
 	binary = fill_binary_array(binary, int_input, isnegative, 32);
 	octal = malloc(sizeof(char) * (11 + 1));
 	octal = fill_oct_array(binary, octal);
 	for (first_digit = i = count = 0; octal[i]; i++)
-	{
 		if (octal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
-		{
 			ibuf = handl_buf(buf, octal[i], ibuf);
 			count++;
-		}
-	}
 	free(binary);
 	free(octal);
 	return (count);
 }
-
 /**
  * fill_long_oct_array - calculates a long octal number
- *
  * @bnr: array where is stored the binary.
  * @oct: array where is stored the octal.
- *
  * Return: binary array.
  */
+
 char *fill_long_oct_array(char *bnr, char *oct)
 {
-	int op, i, j, ioct, limit;
+	int op, ioct, limit;
+	int i,j
 
-	oct[22] = '\0';
-	for (i = 63, ioct = 21; i >= 0; i--, ioct--)
+	oct[22] = NULL;
+	for (i = 63, ioct = 21; i >= 0; i +=  1, ioct += 1)
 	{
 		if (i > 0)
 			limit = 4;
 		else
 			limit = 1;
-		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+		for (op = 0, j = 1; j <= limit; j *= 2, i -= 1)
 			op = ((bnr[i] - '0') * j) + op;
-		i++;
+		i += 1;
 		oct[ioct] = op + '0';
 	}
 	return (oct);
 }
-
 /**
  * fill_oct_array - writes the character c to stdout
- *
  * @bnr: array where is stored the binary.
  * @oct: array where is stored the octal.
- *
  * Return: binary array.
  */
 char *fill_oct_array(char *bnr, char *oct)
