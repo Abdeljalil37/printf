2db472a67ab08dd7a9b51c82ae833398b4f3e6c9
Abdeljalil37 <widadpop@gmail.com>
2023-07-25 17:09:55 +0100

    octal long specifier

--- print_long.c
+++ print_long.c
@@ -83,7 +83,6 @@ int prinlint(va_list arguments, char *buf, unsigned int ibuf)
 	return (i + isneg);
 }
 
-#include "main.h"
 /**
  * prinlunt - prints a long unsigned integer
  * @arguments: number to print
@@ -108,4 +107,48 @@ int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
 		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
 	}
 	return (i);
+}
+
+/**
+ * prinloct - prints long decimal number in octal
+ * @arguments: input number
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int prinloct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(arguments, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 64);
+	octal = malloc(sizeof(char) * (22 + 1));
+	octal = fill_long_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count);
 }
