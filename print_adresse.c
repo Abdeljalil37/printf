70280b7571ff6ea7f25b0f8df038637f743c448d
usri rachid <shixymanflowx@gmail.com>
2023-07-25 02:57:20 -0700

    fix hot

--- print_adresse.c
+++ print_adresse.c
@@ -13,29 +13,18 @@
 int print_add(va_list arguments, char *buf, unsigned int ibuf)
 {
 	void *add;
-	long int int_input;
-	int len, count, first_digit, isnegative;
 	unsigned int i;
-	char *hexadecimal;
 	char* binary;
-	char nill[] = "(nil)";
-
 	add = (va_arg(arguments, void *));
 	if (add == NULL)
+	{
 		i=0;
 		while (nill[i])
 			ibuf = handl_buf(buf, nill[i], ibuf);
 			i++;
 		return (5);
+	}
 	int_input = (intptr_t)add;
-	isnegative = 0;
-		isnegative = 1;
-	binary = malloc(sizeof(char) * (64 + 1));
-	hexadecimal = malloc(sizeof(char) * (16 + 1));
-	ibuf = handl_buf(buf, '0', ibuf);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
-			first_digit = 1;
-			count++;
 	free(binary);
 	free(hexadecimal);
 	return (count + 2);
