e0b3f446c0fc3bf5a12adde345b04ca5ab04b930
usri rachid <shixymanflowx@gmail.com>
2023-07-25 03:09:52 -0700

    fix hot

--- print_oct.c
+++ print_oct.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_oct - prints decimal number in octal
  * @arguments: input number
@@ -9,29 +8,22 @@
  */
 int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_input, i, isnegative, count, first_digit;
-	char *octal, *binary;
 
-	int_input = va_arg(arguments, int);
-	isnegative = 0;
 	if (int_input == 0)
 		ibuf = handl_buf(buf, '0', ibuf);
 		return (1);
 	if (int_input < 0)
 		int_input = (int_input * -1) - 1;
 		isnegative = 1;
-	binary = malloc(sizeof(char) * (32 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 32);
-	octal = malloc(sizeof(char) * (11 + 1));
-	octal = fill_oct_array(binary, octal);
+	binary = calloc(sizeof(char) * (32 + 1));
+	octal = calloc(sizeof(char) * (11 + 1));
 	for (first_digit = i = count = 0; octal[i]; i++)
 		if (octal[i] != '0' && first_digit == 0)
 			first_digit = 1;
-		if (first_digit)
+		else if (first_digit)
 			ibuf = handl_buf(buf, octal[i], ibuf);
 			count++;
 	free(binary);
-	free(octal);
 	return (count);
 }
 /**
@@ -40,7 +32,6 @@ int print_oct(va_list arguments, char *buf, unsigned int ibuf)
  * @oct: array where is stored the octal.
  * Return: binary array.
  */
-
 char *fill_long_oct_array(char *bnr, char *oct)
 {
 	int op, ioct, limit;
